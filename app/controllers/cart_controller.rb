916769a66fbac31190d99e0a56d7d547ec6d0502
Michael E. Nash <utumno86@gmail.com>
2015-06-28 17:15:26 -0400

    working on getting a cart and checkout system going. Still a work in progress

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,23 @@
 class CartController < ApplicationController
+
+  def index
+    @cart = Item.where(item_id: current_user.cart)
+    @total_price = 0
+  end
+
+  def edit
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    current_user.cart << @item.id
+    if current_user.update(cart)
+      redirect_to @item, notice:"Your task was successfully updated"
+    else
+      redirect_to @item, notice: "Your update failed"
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'visitors#index'
   devise_for :users
+  resources :items
+  resources :cart
+  patch '/cart', to: 'cart#update', as: 'update_cart'
 end
