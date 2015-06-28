880c9cf6b54b43f121ea7da08256f2e71b40e971
Michael E. Nash <utumno86@gmail.com>
2015-06-27 22:38:42 -0400

    added some basic view paths

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   def index
     @items = Item.all
