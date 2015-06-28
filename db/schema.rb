201a08910054fddeff0e0b0ad2dafd0f485396e4
Michael E. Nash <utumno86@gmail.com>
2015-06-28 13:23:10 -0400

    added a cart controller and array, work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626212951) do
+ActiveRecord::Schema.define(version: 20150628170559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150626212951) do
     t.string   "name"
     t.integer  "role"
     t.boolean  "is_admin"
+    t.text     "cart",                   default: [],              array: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
