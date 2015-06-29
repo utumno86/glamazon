b898b1ca1e529993baf7060a1b5b865b72723e8a
Michael E. Nash <utumno86@gmail.com>
2015-06-29 00:49:05 -0400

    cart works, output on checkout page weird, may need to be reformatted for Payola, White Whale vanquished

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,30 +1,16 @@
 class CartController < ApplicationController
 
   def index
-    @cart = Item.where(item_id: current_user.cart)
+    @cart = Item.where(id: current_user.cart)
     @total_price = 0
   end
 
-  def show
-
-  end
-
-  def create
-    puts "I am in the create method"
-    @item = Item.find(params[:id])
-    current_user.update_attribute(:cart, current_user.cart << @item.id)
-    redirect_to @item_path, notice:"Your task was successfully updated"
-  end
-
-  def edit
-
-  end
-
   def update
-    puts "I am in the update method"
-    @item = Item.find(params[:id])
-    current_user.update_attribute(:cart, current_user.cart << @item.id)
-    redirect_to @item_path, notice:"Your task was successfully updated"
+    @item = Item.find(params[:item_id])
+    puts @item.name
+    current_user.cart << @item.id
+    current_user.save
+    redirect_to items_path, notice:"Your task was successfully updated"
   end
 
 
